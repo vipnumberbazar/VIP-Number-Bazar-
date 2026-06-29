33b57504a93970529be908d089d75917b5264947
vipnumberbazar <vipnumberbazar.73@gmail.com>
2026-06-29 12:54:05 +0530

    Part 3

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWD14Nf9l7HlFrPMsHijREOHoGrxeehok",
+  authDomain: "vipnumberbazar-73e51.firebaseapp.com",
+  projectId: "vipnumberbazar-73e51",
+  storageBucket: "vipnumberbazar-73e51.firebasestorage.app",
+  messagingSenderId: "756745745147",
+  appId: "1:756745745147:web:e8dcd216eda572c440f65e",
+  measurementId: "G-ZTB2EEZVQ1"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
